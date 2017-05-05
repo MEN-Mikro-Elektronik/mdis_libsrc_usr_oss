f7d5c2f272cf77aa9fdeb8a7e50d0b0deb59aec2
thomas.schnuerer <thomas.schnuerer@men>
2017-05-05 14:47:50 +0200

    added GPL license notification under every MEN document header

--- errno.c
+++ errno.c
@@ -25,8 +25,22 @@
  * Initial Revision
  *
  *---------------------------------------------------------------------------
- * (c) Copyright 2000-2003 by MEN mikro elektronik GmbH, Nuernberg, Germany 
- ****************************************************************************/
+ * (c) Copyright 2000-2003 by MEN mikro elektronik GmbH, Nuremberg, Germany 
+ ******************************************************************************/
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 static const char RCSid[]="$Id: errno.c,v 1.3 2003/06/06 09:29:41 kp Exp $";
 
 #include "uos_int.h"
